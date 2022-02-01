ff6429aa945d4bb468d0bf54647cb15f2a242084
Maxшim Romanenko <djubaiy@gmail.com>
2022-02-01 23:00:01 +0300

    binary search

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-let binarySearch = ()=>{}
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14]
+
+function binarySearch(arr, target) {
+    let low = 0;
+    let high = arr.length - 1;
+    let mid;
+
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2);
+        let guess = arr[mid];
+        if (guess === target) {
+            return mid;
+        }
+        if (guess > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1
+        }
+    }
+
+    return null;
+}
+binarySearch(list,3)
